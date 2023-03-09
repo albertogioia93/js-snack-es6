09c8d34a112076c1c91bd13e4dd6a5a955bdcc2c
albertogioia93 <albertogioia@hotmail.it>
2023-03-09 15:04:59 +0100

    snack3 array bici da corsa + oggetti

--- snack3/main.js
+++ snack3/main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const biciDaCorsa = [
+    {
+        nome: 'graziella',
+        peso: 20,
+    },
+    {
+        nome: 'mountainbike',
+        peso: 15,
+    },
+    {
+        nome: 'bicirubata',
+        peso: 14,
+    },
+    {
+        nome: 'bmx',
+        peso: 10,
+    },
+];
