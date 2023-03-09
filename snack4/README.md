a2f56401da0d68cc759b900d9504b3645a16da94
albertogioia93 <albertogioia@hotmail.it>
2023-03-09 19:59:21 +0100

    valori random a chiavi di oggetti di array

--- snack4/README.md
+++ snack4/README.md
@@ -10,5 +10,6 @@ COME SVOLGO QUESTO ESERCIZIO:
 - Creo un array sotto forma di lista (squadre calcio) e lo assegno ad una variabile 
 - dentro l'array creo degli oggetti che contengono nome della squadra, punti fatti e falli subiti
 - punti fatti e falli subiti li setto di default a  0
+- ciclo sicuro
 - destrutturo gli oggetti estrapolando punti fatti e falli subiti per modificare i dati al loro interno con dei numeri random
 - destrutturo l'array originario per creare un nuovo array contenente degli oggetti che tengono sempre il nome delle squadre e solamente i falli subiti coi nuovi dati generati randomicamente
--- snack4/main.js
+++ snack4/main.js
@@ -1,2 +1,36 @@
 'use strict';
 
+// inizializzo variabile con dentro un array fatto solo di stringhe
+const squadreCalcio = ['milan', 'juventus', 'inter', 'napoli', 'roma'];
+
+// ciclo sull'array creato sopra utilizzando il metodo .map. così facendo faccio diventare ogni stringa di quell'array un oggetto con le varie parole chiave
+const squadreComplete = squadreCalcio.map((element) => {
+    return {
+        nome: element,
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }
+})
+
+// creo la funzione che mi permette di avere numeri random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// inizializzo delle variabili che vanno dentro quella funzione per i numeri random (un numero minimo e uno massimo)
+const puntiMinimi = 1;
+const puntiMassimi = 100;
+
+// ho preso il primo array in alto e ho modificato 2 valori specifici (puntifatti e fallisubiti) degli oggetti al suo interno usando quella funzione per i numeri random
+const secondaLista = squadreComplete.map((element) => {
+    return {
+        nome: element.nome,
+        puntiFatti: getRndInteger(puntiMinimi, puntiMassimi),
+        falliSubiti: getRndInteger(puntiMinimi, puntiMassimi),  
+    }
+});
+
+
+
+
+
