d6eb25500ed4176614003d7eb7d448d317aeb13b
albertogioia93 <albertogioia@hotmail.it>
2023-03-09 19:15:31 +0100

    fine snack3

--- snack3/main.js
+++ snack3/main.js
@@ -20,9 +20,46 @@ const biciDaCorsa = [
     },
 ];
 
+
+let i = 0;
+let pesoMinore = biciDaCorsa[0].peso;
+biciDaCorsa.forEach((element, index) => {
+    if (pesoMinore > element.peso) {
+        pesoMinore = element.peso;
+        i = index;
+    }
+});
+
+console.log(biciDaCorsa[i]);
+
+
+const {nome, peso} = biciDaCorsa[i];
+
+const messaggio = `la bici col nome ${nome} è più leggera di tutte le altre bici della nostra lista perchè pesa ${peso} kg`;
+
+console.log(messaggio);
+
+
+// ALTRO METODO PER SAPERE L'OGGETTO CON PESO MINORE
+
+// let biciLeggera = biciDaCorsa[0];
+// let pesoMinore = biciDaCorsa[0].peso;
+// for (let i = 0; i < biciDaCorsa.length; i++) {
+//     if (pesoMinore > biciDaCorsa[i].peso) {
+//         pesoMinore = biciDaCorsa[i].peso
+//         biciLeggera = biciDaCorsa[i];
+//     }
+// }
+// console.log(biciLeggera);
+
+
+
+
+
+
 // destrutturazione di un array
-const [,,,terzo] = biciDaCorsa;
+// const [,,,terzo] = biciDaCorsa;
 
 // uso del template literal per scrivere un testo in console che comprende la variabile della destrutturazione creata per ricavare la bici più leggera
-console.log(`la bicicletta della lista che ha il peso minore rispetto alle altre, si chiama ${terzo.nome} e pesa ${terzo.peso} kg`);
+// console.log(`la bicicletta della lista che ha il peso minore rispetto alle altre, si chiama ${terzo.nome} e pesa ${terzo.peso} kg`);
 
